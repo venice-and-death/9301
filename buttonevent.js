4c1988f139979d78dfa0cbe6f9a9bb427a0cd4f7
venice-and-death <72038030+venice-and-death@users.noreply.github.com>
2020-11-18 23:07:56 +0800

    Initial commit

---
+++ buttonevent.js
@@ -0,0 +1,10 @@
+ window.onload = function () {
+      document
+        .querySelector("button")
+        .addEventListener("click", function () {
+          // here you can change also a-scene or a-entity properties, like
+          // changing your 3D model source, size, position and so on
+          // or you can just open links, trigger actions...
+          alert("Hi there!");
+        });
+    };
